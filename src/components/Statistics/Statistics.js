6aa1bdfd97443b6f87a01bee2a1c897bbb6e03ea
Taslim <taslimahmed313@gmil.com>
2022-10-12 14:26:45 +0600

    Rechart Responsive

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
+import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 import './Statistics.css';
 
 const Statistics = () => {
@@ -8,12 +8,14 @@ const Statistics = () => {
     return (
       <div>
         <div className="line-chart">
-          <LineChart width={300} height={300}  data={topics.data}>
-            <Line type="monotone" dataKey="total" stroke="#82ca9d" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip></Tooltip>
-          </LineChart>
+          <ResponsiveContainer className={'rechart'} width='100%' height={400}>
+            <LineChart  data={topics.data}>
+              <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip></Tooltip>
+            </LineChart>
+          </ResponsiveContainer>
         </div>
       </div>
     );
